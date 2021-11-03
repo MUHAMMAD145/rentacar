9084a78fa7891194500d50e3d4dd28ed2df2c8f8
Haider Syed <shyder686@gmail.com>
2021-11-03 22:17:26 +0000

    Update BookingSystem.java
    final commenting

--- src/ooc/yoursolution/BookingSystem.java
+++ src/ooc/yoursolution/BookingSystem.java
@@ -19,23 +19,23 @@ public class BookingSystem implements BookingSystemInterface{
     @Override
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
         
-        String input, name;
-        List<Car> cars = new ArrayList<>();
+        String input, name;                   //name defines the company name
+        List<Car> cars = new ArrayList<>();   //input defines the vehicle data from the file
         name = in.readLine();
             
-            while((input = in.readLine())!=null){
+            while((input = in.readLine())!=null){        //definer list splits the data substrings ":" and classify it as make,rate/price and quantity
                 String definer[] = input.split(":");
                 
-                Make make = Make.valueOf(definer[0]);
+                Make make = Make.valueOf(definer[0]);         //using positional indexes in java to define various objects
                 double rate = Double.parseDouble(definer[1]);
                 int numOfCars = Integer.parseInt(definer[2]);
                 
-                for(int i=0;i<numOfCars;i++){
+                for(int i=0;i<numOfCars;i++){       //a simmple for loop fills in the list with different cars and quantity
                     Car car = new Car(i,make,rate);
                     cars.add(car);
                 }
             
-        }
+        }       //returns the instance of RentACar class
             RentACarInterface rentACarInterface = new RentACar(cars,name);
             
             return rentACarInterface;
