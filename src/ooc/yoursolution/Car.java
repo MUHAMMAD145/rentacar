b381da5484a9218cd4b576be002a863e10eefaff
Haider Syed <shyder686@gmail.com>
2021-11-03 09:49:15 +0000

    Update Car.java
    Map Availability for the whole year using HashMaps

--- src/ooc/yoursolution/Car.java
+++ src/ooc/yoursolution/Car.java
@@ -4,6 +4,7 @@
  */
 package ooc.yoursolution;
 
+import java.util.HashMap;
 import java.util.Map;
 import ooc.enums.Make;
 import ooc.enums.Month;
@@ -20,12 +21,35 @@ public class Car implements CarInterface{
     private double rate;
     private Map<Month, Boolean[]>map;
 
+    /*
+    This method creates the availability of the car for the whole year and also defines the car attributes
+    */
     public Car(int id,Make make,double rate){
+        this.id = id;
+        this.make = make;
+        this.rate = rate;
+        createAvailability();
+        
     }
-    @Override
-    public Map<Month, boolean[]> createAvailability() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     
+    @Override
+    public Map createAvailability() { //creating a map to input the enums all at once for better convinience defined and return the map for availability
+     map = new HashMap<>();             
+
+    map.put(Month.JANUARY, new Boolean[31]);
+    map.put(Month.FEBRUARY, new Boolean[28]);
+    map.put(Month.MARCH, new Boolean[31]);
+    map.put(Month.APRIL, new Boolean[30]);
+    map.put(Month.MAY, new Boolean[31]);
+    map.put(Month.JUNE, new Boolean[30]);
+    map.put(Month.JULY, new Boolean[31]);
+    map.put(Month.AUGUST, new Boolean[31]);
+    map.put(Month.SEPTEMBER, new Boolean[30]);
+    map.put(Month.OCTOBER, new Boolean[31]);
+    map.put(Month.NOVEMBER, new Boolean[30]);
+    map.put(Month.DECEMBER, new Boolean[31]);
+
+        return map;
     }
 
     @Override
